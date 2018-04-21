84904d4e134c30f00105f6796826637b0ef62c3b
Patrick Rewers <rewersp@gmail.com>
2018-04-21 05:29:59 -0400

    SVM takes in csv files, checks bad targets without breaking, and returns DataFrames

--- svm.py
+++ svm.py
@@ -1,8 +1,47 @@
 from sklearn import svm
+import pandas as pd
 from sklearn import datasets
 
 # Sample iris dataset
 iris = datasets.load_iris()
 
-# Load .csv file for "nothing" gesture
-nothing = 
+# Define main() function
+def main():
+    # Loop until user finishes entering categories and quits
+    targets = []
+    target = input("Enter target name, or enter \"done\" to quit adding targets: ")
+    while True:
+        if target == 'done':
+            break
+        else:
+            targets.append(target)
+            target = input()
+    for target in targets:
+        try:
+            data = load(target)
+            print(data)
+        except FileNotFoundError:
+            print("File for target \"" + target + "\" not found")
+
+
+def load(target):
+    # Load .csv file for "close" gesture
+    # Add extention
+    target = target + ".csv"
+    # Attempt to open file of category name
+    raw_data = open(target, 'r')
+    # Create list to contain values from file
+    structured_data = []
+    # Add each row from CSV
+    for line in raw_data:
+        line = line[:-1]
+        line = line.split(",")
+        structured_data.append(line)
+    raw_data.close()
+    structured_data = pd.DataFrame(structured_data)
+    # Return final array
+    return structured_data
+
+
+# Call main() function
+main()
