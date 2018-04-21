fcfb146ad3119b219a9809658ae3f3818cd389f5
freelanddavis <barrettfdavis@gmail.com>
2018-04-21 04:08:12 -0400

    Finished training script

--- cereal.py
+++ cereal.py
@@ -5,15 +5,22 @@ import serial
 
 
 #Arduino Serial Read
+
+outF = open('testoutput.txt','w')
+
 ser = serial.Serial('/dev/cu.wchusbserial1410', 115200)
 while True:
     s = ser.readline()
     s = s.decode("utf-8")
-    s = s.rstrip(',')
-    list(s)
+
+    s = s.split(",")
+    s = s[:-1]
+
+    arr = [int(x) for x in s]
+    print(arr)
+    """
     print(s)
-
-
-
+    #print(s, file = outF)
+    """
 
 #Training
--- train_hand.py
+++ train_hand.py
@@ -1,6 +1,7 @@
 import serial
 import keyboard
 import pandas as pd
+import numpy as np
 import time
 import csv
 
@@ -17,20 +18,20 @@ while True:
 
     if command == 'a':  # for training
         trial = []
-        while ((len(trial)) < 99): #store (100 * #inputs) data points
+        while ((len(trial)) < 10): #store (100 * #inputs) data points
 
-            s = ser.readline()              # read in arduino serial output
-            s = s.decode('utf-8')
-            s = s.trim(',')
+              s = ser.readline()
+              s = s.decode("utf-8")
 
+              s = s.split(","); s = s[:-1] # revise later
+              arr = [int(x) for x in s]
 
-
-            trial.append(s) # decode byte literal to string
+              trial.append(arr) # decode byte literal to string
 
         saveQ = input('Save training data? [y/n] ')
         if saveQ == 'y':
             print('Trial data saved to training set')
-            train.append(trial)
+            train = train + trial
         else:
             print('Trial data not saved')
 
@@ -38,11 +39,22 @@ while True:
 
     elif command == ('f'): # for saving training data
 
-        df = pd.DataFrame(train)
         gesture = input('What gesture was that? ')
 
+        """
         with open(gesture + '.csv', 'w') as f:
-            df.to_csv(f, index=False, header=False)
+            aout = np.asarray(train)
+            np.savetxt(f,aout,delimiter = ",")
+
+        gesture = input('What gesture was that? ')
+        df = pd.DataFrame(train)
+        df.to_csv(f, index=False, header=False)
+        #print(df)
+        """
+        with open(gesture + '.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows(train)
+
 
         train = [] #clear out the cache
         print('saved!')
